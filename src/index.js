72a753b892ab5483d0a9696f2e442ac88292f7e0
Stepan Bryl <bryl.step@tut.by>
2022-01-06 16:46:03 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 module.exports = function reverse (n) {
-    let numString = n.toString().split('').reverse().join('');
+    let numString = Math.abs(n).toString().split('').reverse().join('');
     let result = Number(numString);
   return result;
+
 }
